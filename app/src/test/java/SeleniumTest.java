be9db820e96255e84a02dd7e9f61760599908045
ann2303 <anna.ilina.2001@yandex.ru>
2022-04-20 18:51:11 +0300

    test added

--- app/src/test/java/SeleniumTest.java
+++ app/src/test/java/SeleniumTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class SeleniumTest {
@@ -66,11 +67,13 @@ public class SeleniumTest {
     public void setup() {
         wait = new WebDriverWait(driver,10);
         driver.manage().timeouts().pageLoadTimeout(10, SECONDS);
+        driver.manage().window().setSize(new Dimension(1000, 1000));
         System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
     }
 
     @Test
     public void testRootPage() {
+        URL = "http://localhost:8080/";
         driver.get(URL);
         ServicePage servicePage = new ServicePage(driver);
         WebElement name_filter = servicePage.getName_filter();
@@ -87,7 +90,22 @@ public class SeleniumTest {
             String str = row.findElements(By.tagName("td")).get(0).getText();
             assertTrue(str.contains("а"));
         });
+    }
 
+    @Test
+    public void testClientsPage() {
+        URL = "http://localhost:8080/clients";
+        driver.get(URL);
+        WebElement table = wait.until(visibilityOfElementLocated(By.className("table")));
+        List<WebElement> tableRows = table.findElements(By.tagName("tr"));
+        WebElement href = tableRows.get(1).findElements(By.tagName("td")).get(0);
+        href.findElement(By.tagName("a")).click();
+        WebElement address = wait.until(visibilityOfElementLocated(By.name("address")));
+        address.sendKeys("new address");
+        WebElement updateBtn = wait.until(visibilityOfElementLocated(By.id("upd_button")));
+        updateBtn.click();
+
+        assertEquals(driver.getTitle(), "Successful");
     }
 
 
